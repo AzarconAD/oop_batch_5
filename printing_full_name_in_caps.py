6be29afcd54c78c69dccbb71e6fb2b43e7a3c629
AzarconAD <adamdaniel2801@gmai.com>
2025-03-21 23:29:58 +0800

    program 3 done

---
+++ printing_full_name_in_caps.py
@@ -0,0 +1,8 @@
+#ask for full name
+full_name = str(input("Enter your full name: "))
+
+#convert letters into capital letters
+upper_case_name = full_name.upper()
+
+#display
+print(upper_case_name)

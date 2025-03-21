700c39c219f5bba39c4599d90323f1cec0d394bc
AzarconAD <adamdaniel2801@gmai.com>
2025-03-22 00:00:07 +0800

    program 5 done

---
+++ correcting_incorrect_casing.py
@@ -0,0 +1,8 @@
+#ask for full name with incorrect casing
+full_name = str(input("Enter your full name in incorrect casing: "))
+
+#correcting casing
+correct_casing = full_name.title()
+
+#display
+print(correct_casing)

d6ea825591e42113fe475061842eb531ea7398f5
AzarconAD <adamdaniel2801@gmai.com>
2025-03-21 23:42:44 +0800

    program 7 done

--- printing_the_number_of_words_entered.py
+++ printing_the_number_of_words_entered.py
@@ -1,2 +1,8 @@
 #ask user to input a statement
-#
+statement = str(input("Enter a statement: "))
+
+#use split() method to break down statement into words
+word_count = len(statement.split()) #using len() to count the words
+
+#display number of words
+print(word_count)

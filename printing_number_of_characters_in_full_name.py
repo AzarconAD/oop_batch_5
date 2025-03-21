792a6a1db643390bea5db2bbef1711ef3de282bb
AzarconAD <adamdaniel2801@gmai.com>
2025-03-21 23:24:20 +0800

    program 8 done

---
+++ printing_number_of_characters_in_full_name.py
@@ -0,0 +1,8 @@
+#ask user for full namem
+full_name = str(input("Enter your full name: ")).strip() #using strip to not count the spaces
+
+#use len() to count characters in the full name
+char_count = len(full_name)
+
+#display no. of characters
+print(char_count)
